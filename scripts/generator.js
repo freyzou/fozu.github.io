011d7b47a7516e16c7209e3ee29a5d78dfa41c85
freyzou <747852289@qq.com>
2021-05-03 02:40:49 +0800

    asynchronous test

---
+++ scripts/generator.js
@@ -0,0 +1,13 @@
+function* idMaker() {
+    var index = 0;
+    console.log('index' + index);
+    while (true)
+        yield index++;
+}
+
+var gen = idMaker();
+
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+console.log(gen.next().value); // 3
